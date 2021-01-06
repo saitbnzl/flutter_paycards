4587cd412905e63a01adad1cc7b822a9320e0b1f
Sait Banazılı <saitbnzl@gmail.com>
2021-01-06 08:11:21 +0300

    safe area

--- ios/Classes/ViewController.m
+++ ios/Classes/ViewController.m
@@ -25,8 +25,25 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-    _recognizer = [[PayCardsRecognizer alloc] initWithDelegate:delegate recognizerMode:PayCardsRecognizerDataModeNumber|PayCardsRecognizerDataModeName|PayCardsRecognizerDataModeDate resultMode:PayCardsRecognizerResultModeSync container:self.view frameColor:UIColor.cyanColor];
+    UIView *myView = [[UIView alloc] initWithFrame:self.view.bounds];
+    myView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview: myView];
+    if (@available(iOS 11, *)) {
+        UILayoutGuide * guide = self.view.safeAreaLayoutGuide;
+        [myView.leadingAnchor constraintEqualToAnchor:guide.leadingAnchor].active = YES;
+        [myView.trailingAnchor constraintEqualToAnchor:guide.trailingAnchor].active = YES;
+        [myView.topAnchor constraintEqualToAnchor:guide.topAnchor].active = YES;
+        [myView.bottomAnchor constraintEqualToAnchor:guide.bottomAnchor].active = YES;
+    } else {
+        UILayoutGuide *margins = self.view.layoutMarginsGuide;
+        [myView.leadingAnchor constraintEqualToAnchor:margins.leadingAnchor].active = YES;
+        [myView.trailingAnchor constraintEqualToAnchor:margins.trailingAnchor].active = YES;
+        [myView.topAnchor constraintEqualToAnchor:self.topLayoutGuide.bottomAnchor].active = YES;
+        [myView.bottomAnchor constraintEqualToAnchor:self.bottomLayoutGuide.topAnchor].active = YES;
+    }
 
+    [self.view layoutIfNeeded];
+    _recognizer = [[PayCardsRecognizer alloc] initWithDelegate:delegate recognizerMode:PayCardsRecognizerDataModeNumber|PayCardsRecognizerDataModeName|PayCardsRecognizerDataModeDate resultMode:PayCardsRecognizerResultModeSync container:myView frameColor:UIColor.cyanColor];
 }
 
 - (void)viewWillAppear:(BOOL)animated{
