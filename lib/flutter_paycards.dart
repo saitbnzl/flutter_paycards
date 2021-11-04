b6e6fb76e6f40889238d117f440c74ef2958a3ff
Otto Chamo <ochamo@develsystems.com>
2021-11-04 11:33:29 -0600

    fix: Add null safety support

--- lib/flutter_paycards.dart
+++ lib/flutter_paycards.dart
@@ -6,7 +6,7 @@ class FlutterPaycards {
   static const MethodChannel _channel =
   const MethodChannel('flutter_paycards');
 
-  static Future<dynamic> startRecognizer({bool fullScreen:false, String cancelLabel}) async {
+  static Future<dynamic> startRecognizer({bool fullScreen:false, String cancelLabel ="Cancel"}) async {
     final dynamic cardsScanResult = await _channel.invokeMethod('startRecognizer', <String, dynamic>{
       'fullScreen': fullScreen,
       'cancelLabel': cancelLabel
